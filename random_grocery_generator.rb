0cd40952187a56d4fc232105adf1fc899ec84ae1
katy@karstaway.com <katy@karstaway.com>
2018-08-31 22:25:39 +0000

    automatically backed up by learn

--- random_grocery_generator.rb
+++ random_grocery_generator.rb
@@ -57,5 +57,5 @@ end
 puts "Your total is #{checkout(cart: cart, coupons: coupons)}"
 
 def consolidate_cart (items)
-  items.group_by { |item, atributes| attributes[:count => item.size] }
+  items.each { |item, atributes| attributes[:count => item.size] }
    end
